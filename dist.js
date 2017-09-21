c20b61035419836245c2bfbbf03c50e4b0456fa6
Simon Oulevay <simon.oulevay@gmail.com>
2017-09-21 11:49:43 +0200

    Init command

--- dist.js
+++ dist.js
@@ -1,6 +1,37 @@
+const chalk = require('chalk');
+const del = require('del');
+const fs = require('fs-extra');
+const ora = require('ora');
 const path = require('path');
 
-module.exports = function(entryPointPath, bundlePath) {
+const { runWebpack } = require('./lib/utils');
+
+compileClient()
+  .catch(err => console.warn(err));
+
+async function compileClient(config) {
+
+  const distDir = path.join(__dirname, 'dist');
+  await del(distDir);
+
+  await fs.mkdirs(distDir);
+
+  const entryPointSourcePath = path.join(__dirname, 'src', 'index.js');
+  const bundlePath = path.join(distDir, 'client.js');
+
+  const compiling = runWebpack(createWebpackConfig(entryPointSourcePath, bundlePath));
+
+  const spinner = ora('Compiling client...').start();
+  await compiling.catch(err => {
+    spinner.fail();
+    return Promise.reject(err);
+  });
+
+  spinner.stop();
+  ora(`Compiled client to ${chalk.magenta('dist')}`).succeed();
+}
+
+function createWebpackConfig(entryPointPath, bundlePath) {
   return {
     entry: entryPointPath,
     module: {
---
+++ lib/templates/SUBJECT.md
@@ -0,0 +1,3 @@
+# Courses <img src="./markdown.svg" width=48.75 height=30 alt="MD">
+
+Write courses in Markdown and publish them as HTML slides.
