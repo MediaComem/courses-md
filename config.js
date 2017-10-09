4918b3e541980ec4124a9fae79eb700b202d6c7e
Simon Oulevay <simon.oulevay@gmail.com>
2017-10-09 16:26:32 +0200

    Update publish url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.title = 'Courses MD';
 exports.build = 'docs/md';
 exports.publish = {
-  url: 'git@github.com:AlphaHydrae/courses-md.git',
+  url: 'git@github.com:MediaComem/courses-md.git',
   branch: 'gh-pages',
   version: '2017',
   afterBuild: async function() {
