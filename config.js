7c3905b1aa38ebb97cf11c9767cdd31bcc36d44b
Simon Oulevay <simon.oulevay@gmail.com>
2017-10-09 16:46:12 +0200

    Config: after build hook

--- config.js
+++ config.js
@@ -1,10 +1,34 @@
+const del = require('del');
+const fs = require('fs-extra');
+const path = require('path');
+
+const { execute } = require('./lib/utils');
+
 exports.title = 'Courses MD';
 exports.build = 'docs/md';
+
 exports.publish = {
   url: 'git@github.com:MediaComem/courses-md.git',
   branch: 'gh-pages',
   version: '2017',
-  afterBuild: async function() {
-    console.log('@@@ after build');
-  }
+  afterBuild: afterBuild
 };
+
+async function afterBuild(config, tmpDir) {
+  if (!tmpDir) {
+    throw new Error(`Temporary directory is required, got ${tmpDir}`);
+  }
+
+  const esdocPath = path.join(tmpDir, 'api');
+  await del(esdocPath, { force: true });
+
+  const esdocConfig = require('./.esdoc.json');
+  esdocConfig.destination = esdocPath;
+
+  const tmpEsdocConfig = path.join(tmpDir, '.esdoc.json');
+  await fs.writeFile(tmpEsdocConfig, JSON.stringify(esdocConfig), 'utf8');
+
+  await execute([ './node_modules/.bin/esdoc', '-c', tmpEsdocConfig ]);
+
+  await del(tmpEsdocConfig, { force: true });
+}
